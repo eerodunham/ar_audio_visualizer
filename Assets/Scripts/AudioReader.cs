66bdca8310d1d1c059e3d55a0a2093a5ea75593b
Matthew M. <blazinggunner@hotmail.com>
2025-11-04 14:42:49 -0600

    Adding separate MicrophoneDevice file, adding FFT lib ComputeFFT

--- Assets/Scripts/AudioReader.cs
+++ Assets/Scripts/AudioReader.cs
@@ -1,128 +1,10 @@
-using System;
-using System.Runtime.CompilerServices;
-using System.Reflection;
-
 using Unity.Collections;
+
+using Unity.Mathematics;
 
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
-using Unity.Mathematics;
-using Unity.Collections.LowLevel.Unsafe;
-
-public static class MicrophoneQueryMethods
-{
-    private delegate int GetMicrophoneDeviceIDLambda(string name);
-    private delegate AudioClip StartRecordLambda(int deviceID, bool loop, float lengthSec, int frequency);
-    private delegate void StopRecordLambda(int deviceID);
-    private delegate bool IsRecordingLambda(int deviceID);
-    private delegate int  GetRecordPositionLambda(int deviceID);
-
-    private static GetMicrophoneDeviceIDLambda microphoneDeviceIDLambda;
-    private static StartRecordLambda startRecordLambda;
-    private static StopRecordLambda  stopRecordLambda;
-    private static IsRecordingLambda isRecordingLambda;
-    private static GetRecordPositionLambda getRecordPositionLambda;
-
-    private static T GetPrivateStaticFunc<T>(string name) where T : Delegate
-    {
-        MethodInfo microphoneIDInfo = typeof(Microphone).GetMethod(name, BindingFlags.NonPublic | BindingFlags.Static);
-        return (T) Delegate.CreateDelegate(typeof(T), microphoneIDInfo);
-    }
-
-    static MicrophoneQueryMethods()
-    {
-        microphoneDeviceIDLambda = GetPrivateStaticFunc<GetMicrophoneDeviceIDLambda>("GetMicrophoneDeviceIDFromName");
-        startRecordLambda        = GetPrivateStaticFunc<StartRecordLambda>("StartRecord");
-        stopRecordLambda         = GetPrivateStaticFunc<StopRecordLambda>("EndRecord");
-        isRecordingLambda        = GetPrivateStaticFunc<IsRecordingLambda>("IsRecording");
-        getRecordPositionLambda  = GetPrivateStaticFunc<GetRecordPositionLambda>("GetRecordPosition");
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int GetMicrophoneDeviceID(string name) => microphoneDeviceIDLambda.Invoke(name);
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static AudioClip StartRecording(int deviceID, bool loop, float lengthSec, int frequency) => startRecordLambda.Invoke(deviceID, loop, lengthSec, frequency);
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void StopRecording(int deviceID)    => stopRecordLambda.Invoke(deviceID);
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static bool IsRecording(int deviceID)      => isRecordingLambda.Invoke(deviceID);
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int GetRecordPosition(int deviceID) => getRecordPositionLambda.Invoke(deviceID);
-}
-
-public sealed class MicrophoneDevice : IDisposable
-{
-    private readonly string deviceName;
-    private readonly int    cachedID;
-    private readonly int    samples;
-    private AudioClip       audioClip;
-
-    private int currentSample;
-
-    private NativeArray<float>  sampledData;
-    private NativeArray<float2> complexData;
-
-    private FFTProperties props;
-
-    public string DeviceName       => deviceName;
-    public int Samples             => samples;
-    public NativeArray<float> Data => sampledData;
-
-    private MicrophoneDevice()
-    {
-        deviceName  = null;
-        audioClip   = null;
-        cachedID    = -1;
-        samples     = 0;
-
-        sampledData = default;
-        complexData = default;
-    }
-
-    public MicrophoneDevice(string deviceName, int samples = 44100)
-    {
-        Debug.Assert(deviceName != null && deviceName.Length > 0);
-        Debug.Assert(samples > 0);
-        this.deviceName = deviceName;
-        this.samples    = samples;
-
-        cachedID = MicrophoneQueryMethods.GetMicrophoneDeviceID(deviceName);
-        Debug.Assert(cachedID != -1);
-
-        sampledData = new NativeArray<float>(samples,  Allocator.Persistent);
-        complexData = new NativeArray<float2>(samples, Allocator.Persistent);
-
-        UnsafeText text = new(deviceName.Length, Allocator.Temp);
-        text.Append($"{samples}Wisdom");
-        bool isSuccessful = FFTProperties.Create(sampledData.AsReadOnlySpan(), complexData.AsReadOnlySpan(), text, out props);
-        Debug.Assert(isSuccessful);
-    }
-
-    public void Start()
-    {
-        audioClip = MicrophoneQueryMethods.StartRecording(cachedID, true, 1, samples);
-    }
-
-    public void End() => MicrophoneQueryMethods.StopRecording(cachedID);
-
-    public void Write()
-    {
-        audioClip.GetData(sampledData.AsSpan(), offsetSamples: currentSample);
-        currentSample = MicrophoneQueryMethods.GetRecordPosition(cachedID);
-    }
-
-    public void Dispose()
-    {
-        audioClip = null;
-
-        sampledData.Dispose();
-        sampledData = default;
-
-        complexData.Dispose();
-        complexData = default;
-    }
-}
 
 [DisallowMultipleComponent]
 public sealed class AudioReader : MonoBehaviour
@@ -140,8 +22,6 @@ public sealed class AudioReader : MonoBehaviour
     private Image imageUI;
 
     private int gridDim = 1024 / 128;
-
-    private NativeArray<float2> complex;
 
     void Start()
     {
--- Assets/Scripts/FFT/FFTLib.cs
+++ Assets/Scripts/FFT/FFTLib.cs
@@ -11,14 +11,19 @@ using UnityEngine;
 [BurstCompile]
 public struct FFTProperties
 {
-    [DllImport("AudioVisFFT.dll")]
+    [DllImport("AudioVisFFT")]
     private static unsafe extern bool CreateFFTProps(float* signalPtr, float2* complexPtr, ulong length, char* fileName, ref FFTProperties props);
+
+    [DllImport("AudioVisFFT")]
+    private static unsafe extern void ComputeFFT(ref FFTProperties props);
 
     private unsafe float*  signalPtr;
     private unsafe float2* complexPtr;
     private ulong length;
 
-    public static unsafe bool Create<T>(in ReadOnlySpan<float> signal, in ReadOnlySpan<float2> complex, T str, out FFTProperties props) where T : unmanaged, IUTF8Bytes
+    public unsafe readonly bool IsValid => signalPtr != null && complexPtr != null && length > 0;
+
+    public static unsafe bool TryCreate<T>(in ReadOnlySpan<float> signal, in ReadOnlySpan<float2> complex, T str, out FFTProperties props) where T : unmanaged, IUTF8Bytes
     {
         props = default;
         if (Hint.Unlikely(signal.Length == 0 || signal.Length != complex.Length))
@@ -33,9 +38,14 @@ public struct FFTProperties
             }
         }
     }
-}
 
-public struct FFTDispatch
-{
-
+    public unsafe bool ComputeFFT()
+    {
+        if (Hint.Unlikely(!IsValid))
+        {
+            return false;
+        }
+        ComputeFFT(ref this);
+        return true;
+    }
 }
